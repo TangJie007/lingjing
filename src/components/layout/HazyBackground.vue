e241642278593a97870878d98d04b51edeae9939
tangjie <981955667@qq.com>
2026-08-18 23:20:46 +0800

    chore: 移除 wetspec skill 文档与模板

--- src/components/layout/HazyBackground.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="hazy-gradient-bg" aria-hidden="true">
-    <div class="hazy-blob blob-1" />
-    <div class="hazy-blob blob-2" />
-    <div class="hazy-blob blob-3" />
-  </div>
-</template>
